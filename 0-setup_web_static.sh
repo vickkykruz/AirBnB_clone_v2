4833201468d3b95d384bca4eaf0c68703d1c4f93
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-12-06 16:05:58 -0800

    testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,19 +3,19 @@
 
 # Install nginx if it is not install
 if ! command -v nginx &> /dev/null; then
-   sudo apt-get update
-   sudo apt-get install -y nginx
+	sudo apt-get update
+	sudo apt-get install -y nginx
 fi
 
 # Create the important folder
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 
 # Create the fake HTML File
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t<h1>Holberton School</h1>\n\t</body>\n\t</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Remove the existing symbolic link if it exist
 if [ -L /data/web_static/current ]; then
-   sudo rm /data/web_static/current
+	sudo rm /data/web_static/current
 fi
 
 # Create the symbolic link
@@ -25,15 +25,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the nginx configuration
-nginx_config="/etc/nginx/sites-enabled/default"
-if sudo sed -i '/location \/hbnb_static {/a \
-    alias /data/web_static/current/;\
-    index index.html index.htm;' "$nginx_config"; then
-    echo "Nginx configuration updated successfully."
-else
-    echo "Error: Failed to update Nginx configuration."
-    exit 1
-fi
+sudo sed -i "s/^\s*location \/ {/\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tindex index.html index.htm;\n\t}\n\n&/" /etc/nginx/sites-enabled/default
 
 # Restart the nginx
 sudo service nginx restart
