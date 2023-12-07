1f079b94b83bcf7b712b0392cc115b9f800e2ee8
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-12-07 02:11:06 -0800

    Testing Process...

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,9 @@ Fabric script (based on the file 1-pack_web_static.py) that distributes an
 archive to your web servers, using the function do_deploy:
 """
 
-from fabric.api import *
+from fabric.api import local, put, run, env
 from datetime import datetime
-import os
+from os.path import isfile
 
 
 env.hosts = ["54.172.239.29", "54.237.98.187"]
