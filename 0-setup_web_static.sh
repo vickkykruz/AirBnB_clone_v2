f7b668956151fc0ef1ad59f446aa1a275f94c9f6
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-12-07 00:31:05 -0800

    Checker testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,21 @@
 # This is a script that sets up your web servers for the deployment of web_static.
 
 # Install nginx if it is not install
-if ! command -v nginx &> /dev/null; then
-	sudo apt-get update
-	sudo apt-get install -y nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 
 # Create the important folder
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 
 # Create the fake HTML File
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t<h1>Holberton School</h1>\n\t</body>\n\t</html>" | sudo tee /data/web_static/releases/test/index.html
+echo -e "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Remove the existing symbolic link if it exist
 if [ -L /data/web_static/current ]; then
@@ -29,5 +34,3 @@ sudo sed -i "s/^\s*location \/ {/\tlocation \/hbnb_static {\n\t\talias \/data\/w
 
 # Restart the nginx
 sudo service nginx restart
-
-exit 0
